b4f32a019cff2bd4eff8c2a171b03ed926643be4
Vyadavgit <>
2022-01-26 13:25:53 -0600

    day 1

--- src/day1/Welcome.java
+++ src/day1/Welcome.java
@@ -3,7 +3,7 @@ package day1;
 public class Welcome {
 
     public static void main(String[] args) {
-        String greeting = "Welcome to Core Java! ";
+        String greeting = "Welcome to Core Java!  ";
         System.out.println(greeting);
         for (int i = 0; i < greeting.length(); i++) {
             System.out.print("*");

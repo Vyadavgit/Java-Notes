0553980e78965973b1aefdaf50eaae8887fae07e
Vyadavgit <>
2022-01-26 16:25:57 -0600

    day2

---
+++ src/day2/Laptop.java
@@ -0,0 +1,21 @@
+package day2;
+
+public class Laptop {
+    //States
+
+    // color
+    // model
+    // operatingSystem
+    // brand
+    // processorType
+    // serialNumber
+    // isTouchScreen
+    // price
+    // screenSize
+
+    //Behaviours
+
+    // starting
+    // shuttingDown
+    // playingMovie
+}

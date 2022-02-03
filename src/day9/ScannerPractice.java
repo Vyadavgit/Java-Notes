50887f20e4b2c66bb72215dcca90854e4ef86be1
Vyadavgit <>
2022-02-02 23:08:04 -0600

    Exception handling

--- src/day9/ScannerPractice.java
+++ src/day9/ScannerPractice.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 import java.util.logging.Logger;
 
 public class ScannerPractice {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
